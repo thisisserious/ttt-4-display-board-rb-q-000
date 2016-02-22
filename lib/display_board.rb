a1a2908a3d6ef1cf5bc40a5b79dfaffdebc115d3
thisisserious <laura.l.abend@gmail.com>
2016-02-22 02:45:37 +0000

    nitrous-backup at Mon Feb 22 02:45:37 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+display = ["x", "o"]
+def display_board(display)
+  puts  " #{display[0]}  | #{display[0]}  | #{display[1]}  "
+  puts  "-----------"
+  puts  " #{display[1]}  | #{display[0]}  | #{display[1]}  "
+  puts  "-----------"
+  puts  " #{display[1]}  | #{display[0]}  | #{display[0]}  "
+end
